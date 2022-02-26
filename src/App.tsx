833c8e6984fac8e1db3fdb6782a59b5cddf99933
nemutas <inohiro.focru@gmail.com>
2022-02-26 18:00:10 +0900

    add link button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { VFC } from 'react';
+import { LinkIconButton } from './LinkIconButton';
 import { TCanvas } from './three/TCanvas';
 
 export const App: VFC = () => {
 	return (
 		<div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
 			<TCanvas />
+			<LinkIconButton imagePath="/assets/icons/github.svg" linkPath="https://github.com/nemutas/r3f-gyogun" />
 		</div>
 	)
 }
